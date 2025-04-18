e40101e3dc7ed7345a4cb67d0e18231369c3d05c
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2025-04-18 16:46:00 +0200

    - add analysis logs - fix logos

---
+++ .changeset/wide-signs-tell.md
@@ -0,0 +1,7 @@
+---
+'@platforma-open/milaboratories.immuno-match.workflow': patch
+'@platforma-open/milaboratories.immuno-match.model': patch
+'@platforma-open/milaboratories.immuno-match.ui': patch
+---
+
+Add logs; fixed logos
--- ui/src/pages/MainPage.vue
+++ ui/src/pages/MainPage.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
-import { PlDropdownRef } from '@platforma-sdk/ui-vue';
-import { GraphMaker } from '@milaboratories/graph-maker';
 import type { PredefinedGraphOption } from '@milaboratories/graph-maker';
+import { GraphMaker } from '@milaboratories/graph-maker';
+import { PlBtnGhost, PlDropdownRef, PlLogView, PlSlideModal } from '@platforma-sdk/ui-vue';
+import { ref, watch } from 'vue';
 import { useApp } from '../app';
-import { watch } from 'vue';
 
 const app = useApp();
+
+const defaultOptions: PredefinedGraphOption<'histogram'>[] = [{
+  inputName: 'value',
+  selectedSource: {
+    kind: 'PColumn',
+    name: 'pl7.app/immunomatch/pairing_score',
+    valueType: 'Double',
+    axesSpec: [
+      {
+        name: 'pl7.app/vdj/scClonotypeKey',
+        type: 'String',
+      },
+    ],
+  },
+}];
+const logsOpen = ref(false);
 
 watch(() => app.model.args.inputHeavy, (newVal) => {
   if (newVal === undefined) {
@@ -20,23 +36,17 @@ watch(() => app.model.args.inputHeavy, (newVal) => {
     v-model="app.model.ui.graphStateHistogram"
     chartType="histogram"
     :p-frame="app.model.outputs.predictions"
-    :default-options="[{
-      inputName: 'value',
-      selectedSource: {
-        kind: 'PColumn',
-        name: 'pl7.app/immunomatch/pairing_score',
-        valueType: 'Double',
-        axesSpec: [
-          {
-            name: 'pl7.app/vdj/scClonotypeKey',
-            type: 'String',
-          },
-        ],
-      },
-    }] satisfies PredefinedGraphOption<'histogram'>[]"
+    :default-options="defaultOptions"
   >
+    <template #titleLineSlot>
+      <PlBtnGhost @click.stop="logsOpen = true">Show logs</PlBtnGhost>
+    </template>
     <template #settingsSlot>
       <PlDropdownRef v-model="app.model.args.inputHeavy" :options="app.model.outputs.heavyInputOptions" label="Heavy Input" />
     </template>
   </GraphMaker>
+  <PlSlideModal v-model="logsOpen" width="800px" closeOnOutsideClick>
+    <template #title>Logs</template>
+    <PlLogView :log-handle="app.model.outputs.log" />
+  </PlSlideModal>
 </template>
